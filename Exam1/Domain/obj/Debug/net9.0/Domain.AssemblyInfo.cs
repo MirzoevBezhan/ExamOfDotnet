c5c4ca64453a9670e20f1fd7ba70ed251d65d7c9
Bezhan_Mirzoev <assas1ndarknesss@gmail.com>
2025-04-30 15:36:01 +0500

    addd

--- Exam1/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Exam1/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83144a8588d29316657da3a2c564debb345e9e8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17d1ba72c00f5795e8739905d3db4024b9e8563e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
